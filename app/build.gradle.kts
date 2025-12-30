2d311bf770eb0a73aa6fa5f5cd865495f15d0c85
Saad Ullah Khan <khansaad45678900@gmail.com>
2025-12-30 10:53:20 +0300

    change build number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.app.nosatmosphereeffect"
         minSdk = 36 // Android 16+
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "2.0.1"
     }
 
     buildFeatures {
