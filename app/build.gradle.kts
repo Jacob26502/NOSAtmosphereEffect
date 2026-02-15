5151f17e93f787b82f54ce89763592cc48ed991a
Saad Ullah Khan <khansaad45678900@gmail.com>
2026-02-15 06:00:31 +0300

    v5.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "com.saad_khan_rind.atmosphere_effect"
-        versionName = "5.0.0"
-        versionCode = 100032
+        versionName = "5.0.1"
+        versionCode = 100033
     }
 
     flavorDimensions += "apiLevel"
@@ -21,14 +21,14 @@ android {
             dimension = "apiLevel"
             minSdk = 36
             targetSdk = 36
-            versionCode = 200032
+            versionCode = 200033
         }
 
         create("v33") {
             dimension = "apiLevel"
             minSdk = 33
             targetSdk = 33
-            versionCode = 100032
+            versionCode = 100033
         }
 
     }
