147fa4249a175b423b4d668253525d5e737c3d42
Saad Ullah Khan <khansaad45678900@gmail.com>
2026-03-19 05:07:37 +0300

    change version code.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "com.saad_khan_rind.atmosphere_effect"
-        versionName = "5.4.3"
-        versionCode = 100543
+        versionName = "5.5.0"
+        versionCode = 100550
     }
 
     flavorDimensions += "apiLevel"
@@ -21,14 +21,14 @@ android {
             dimension = "apiLevel"
             minSdk = 36
             targetSdk = 36
-            versionCode = 200543
+            versionCode = 200550
         }
 
         create("v33") {
             dimension = "apiLevel"
             minSdk = 33
             targetSdk = 33
-            versionCode = 100543
+            versionCode = 100550
         }
 
     }
